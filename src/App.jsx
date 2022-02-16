7fb3f2d7209c24a01d2c85a6d4568a8537762ed5
Juan Manuel Roig Courtis <jmroigcourtis.dev@gmail.com>
2022-02-16 12:18:48 -0300

    Primer subida de NAV

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './NavBar/NavBar';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <NavBar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return <nav id="Nav">
+                <ul>
+                    <li><a href="#"><p>Menu</p></a></li>
+                    <li><a href="#"><p>Productos</p></a></li>
+                    <li><a href="#"><p>Sucursales</p></a></li>
+                </ul>
+            </nav>
+}
+
+export default Navbar;
