4ea144fe539e6ba48a6654b9a2f4cfb18278f591
Juan Manuel Roig Courtis <jmroigcourtis.dev@gmail.com>
2022-03-05 13:59:48 -0300

    Corrección ortográfica

--- src/Components/Helper/getFetch.js
+++ src/Components/Helper/getFetch.js
@@ -4,9 +4,9 @@ import LukeSkyWalkerGrogu from '../Images/LukeSkyWalkerGrogu.jpg'
 import DrStrangeWhatIf from '../Images/DrStrangeWhatIf.jpg'
 
 const productos = [
-            {id : 1, title : "IronMan EndGame", description : 'Funko Pop representativo de la pelea entre IronMan y Thanos', price: '1200', pictureURL: IronMan},
+            {id : 1, title : "Iron Man Endgame", description : 'Funko Pop representativo de la pelea entre IronMan y Thanos', price: '1200', pictureURL: IronMan},
             {id : 2, title : "Harry Potter y Hedwig ", description : 'Funko Pop representativo de Harry Potter y Hedwig', price: '1200', pictureURL: HarryPotterOwl},
-            {id : 3, title : "Luke SkyWalker y Grogu", description : 'Funko Pop representativo de la serie "The Mandalorian" ', price: '1200', pictureURL: LukeSkyWalkerGrogu},
+            {id : 3, title : "Luke Skywalker y Grogu", description : 'Funko Pop representativo de la serie "The Mandalorian" ', price: '1200', pictureURL: LukeSkyWalkerGrogu},
             {id : 4, title : "Dr.Strange What If", description : 'Funko Pop representativo del Dr.Strange en la serie "What If"', price: '1200', pictureURL: DrStrangeWhatIf }
 
     ]
